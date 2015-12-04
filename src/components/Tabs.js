c92a575a409e9af238381ab89c3ccc95e8445d8e
Andy Bangs <andrew.yuta@gmail.com>
2015-12-04 09:30:50 -0800

    add state to Tabs

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -2,11 +2,22 @@ import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
 class Tabs extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state= { selected: 1 }
+  }
   render() {
+    const { selected } = this.state
+
     return (
       <div style={styles.container}>
-        <Link to="/coffee" style={styles.tab}>coffee</Link>
-        <Link to="/water" style={styles.tab} >water</Link>
+        <Link to="/coffee" style={styles.tab} onClick={() => this.setState({ selected: 1 })}>
+          <span style={selected === 1 ? styles.active : styles.inactive}>coffee</span>
+        </Link>
+        <Link to="/water" style={styles.tab} onClick={() => this.setState({ selected: 2 })}>
+          <span style={selected === 2 ? styles.active : styles.inactive}>water</span>
+        </Link>
       </div>
     )
   }
@@ -26,6 +37,13 @@ const styles = {
     color: 'black',
     fontSize: 25,
     textAlign: 'center'
+  },
+  active: {
+    color: 'black',
+    fontSize: '1.2em'
+  },
+  inactive: {
+    color: 'grey'
   }
 }
 
